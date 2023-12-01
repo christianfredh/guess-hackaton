24f90fef6aed07fcaf6aa3030af60fc67fdf6823
Andreas Jangnäs <andreas.jangnas@earlybird.se>
2023-12-01 14:42:33 +0100

    Temp commit for grids

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
+import { Players } from "./players";
 
 export default function Home() {
   return (
     <>
-      <h1 className="uppercase">Highscore</h1>
+      <div className="grid-cols-8"></div>
+      <div className="grid-cols-4">
+        <Players />
+      </div>
     </>
   );
 }
---
+++ src/app/game/players.tsx
@@ -0,0 +1,7 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+
+export const Players = () => {
+  return <></>;
+};
