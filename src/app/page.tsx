f1f575bdbc73e8b8d9342008b3db9fd130ee5f92
Christian Fredh <christian.fredh@gmail.com>
2023-12-01 11:45:47 +0100

    SUBJECT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
-
 export default async function Home() {
-  const base64image: string = await fetch(process.env.URL + "/api/image?promt=balloon")
+  const subject: string = await fetch(process.env.URL + "/api/subject")
     .then((res) => res.json())
-    .then((data) => data.data.openai.items[0].image);
+    .then((data) => data.subject);
 
-  console.log(base64image);
+  const promt = "a clue for a quiz game for a " + subject;
+
+  const base64image: string = await fetch(
+    process.env.URL + "/api/image?promt=" + promt
+  )
+    .then((res) => res.json())
+    .then((data) => data?.data?.openai?.items[0]?.image);
+
+
+
+  console.log(promt);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
