b2c148b1a9560ea9599c99a442d15a042fb26382
Andreas Jangnäs <andreas.jangnas@earlybird.se>
2023-12-01 14:52:59 +0100

    Added players

--- src/app/game/players.tsx
+++ src/app/game/players.tsx
@@ -2,6 +2,31 @@
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 
+type Player = {
+  name: string;
+  score: number;
+};
+
 export const Players = () => {
-  return <></>;
+  const players: Player[] = [
+    { name: "karl", score: 0 },
+    { name: "anja", score: 0 },
+    { name: "tomp", score: 0 },
+    { name: "chri", score: 0 },
+    { name: "anna", score: 0 },
+    { name: "nils", score: 0 },
+  ];
+
+  return (
+    <>
+      <h2 className="uppercase font-extrabold">deltagare {players.length}</h2>
+      <ul className="uppercase list-none">
+        {players.map((player, index) => (
+          <li key={index}>
+            {player.name}: {player.score}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
