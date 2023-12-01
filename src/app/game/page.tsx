5914d3e77d5b334f98033fe92457d2577a333d39
Andreas Jangnäs <andreas.jangnas@earlybird.se>
2023-12-01 14:59:51 +0100

    adjusted grid system

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -5,11 +5,13 @@ import { Players } from "./players";
 
 export default function Home() {
   return (
-    <>
-      <div className="grid-cols-8"></div>
-      <div className="grid-cols-4">
+    <div className="grid grid-cols-1 sm:grid-cols-5">
+      <div className="col-span-1 sm:col-span-4 p-4">
+        <p>Testar att se huvudinnehåll </p>
+      </div>
+      <div className="col-span-1 sm:col-span-1 p-4">
         <Players />
       </div>
-    </>
+    </div>
   );
 }
